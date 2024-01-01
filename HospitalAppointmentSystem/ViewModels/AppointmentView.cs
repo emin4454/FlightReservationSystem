7ac7a99758ffbe9c12b78b6bfd188e2f78bb94a6
emin4454 <132940987+emin4454@users.noreply.github.com>
2024-01-01 22:30:55 +0300

    xddsacdd

--- HospitalAppointmentSystem/ViewModels/AppointmentView.cs
+++ HospitalAppointmentSystem/ViewModels/AppointmentView.cs
@@ -5,7 +5,6 @@ namespace HospitalAppointmentSystem.ViewModels
     public class AppointmentView
     {
         public int doctorId { get; set; }
-        public string userId { get; set; }
         public DateTime appointmentDate { get; set; } 
         public string appointmentTime { get; set; } // 0--5
     }
